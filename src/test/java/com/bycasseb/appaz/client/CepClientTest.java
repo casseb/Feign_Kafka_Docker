a934a64283166bceaee0836078fa03f448a84e36
felipe-casseb <felipe.casseb@gmail.com>
2019-05-16 11:45:51 -0300

    Adicionado testes de integração

--- src/test/java/com/bycasseb/appaz/client/CepClientTest.java
+++ src/test/java/com/bycasseb/appaz/client/CepClientTest.java
@@ -4,14 +4,12 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.RANDOM_PORT)
-@DirtiesContext
 public class CepClientTest {
 
     @Autowired
