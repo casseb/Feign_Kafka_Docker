c37cf53920e6804b7b153d680cb2b0949ccd618f
felipe-casseb <felipe.casseb@gmail.com>
2019-05-17 19:18:45 -0300

    Testando kafka

--- src/main/java/com/bycasseb/appaz/listener/EnderecoListener.java
+++ src/main/java/com/bycasseb/appaz/listener/EnderecoListener.java
@@ -7,13 +7,22 @@ import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
+import java.util.concurrent.CountDownLatch;
+
 @Component
 @Slf4j
 public class EnderecoListener {
 
+    private CountDownLatch latch = new CountDownLatch(1);
+
+    public CountDownLatch getLatch() {
+        return latch;
+    }
+
     @StreamListener(EnderecoStream.INPUT)
     public void hendleEndereco(@Payload Endereco endereco){
         log.info("Endereço recebido: {}",endereco);
+        latch.countDown();
     }
 
 }
